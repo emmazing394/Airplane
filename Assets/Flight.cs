2fe207d71901493ccafc70adb5bee70f9aed18d5
emmazing394 <33235972+emmazing394@users.noreply.github.com>
2020-06-04 22:53:21 +0200

    Add files via upload

---
+++ Assets/Flight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flight : MonoBehaviour
+{
+    public float speed = 80.56f;
+
+    //private Rigidbody planeRigidbody;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("flight added to: " + gameObject.name);
+
+       // planeRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        //turnInput = Input.GetAxis("Horizontal");
+
+        transform.position += transform.forward * Time.deltaTime * speed;
+
+        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+
+        float heightGround = Terrain.activeTerrain.SampleHeight(transform.position);
+
+        if (heightGround > transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, heightGround, transform.position.z);
+        }
+
+    }
+
+}
